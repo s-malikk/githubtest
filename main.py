e585843d42c668fb6889f348b6627cc2e8e630d5
Nikita <115031071+nikitawarrier@users.noreply.github.com>
2023-06-15 19:17:09 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+int1= int(input("Enter a number: "))
+int2 = int(input("Enter another number: "))
+sum = str(int1 + int2)
+print("\n" + sum)
+
+num1 = "hi"
+num2 = "bye"
+sum2 = str(num1 + num2)
+print("\n" + sum2)
+
+
+
